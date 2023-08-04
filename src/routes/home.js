49b17197694d813c1f6e67b844c3f556c3989620
anandh4411 <anandh4411@gmail.com>
2023-08-04 22:01:53 +0530

    home.js - test update for env var

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ message: "Hey, this is vision-engine's User Auth Microservice" });
+  res.send({
+    message:
+      "Hey, this is vision-engine's User Auth Microservice: " +
+      process.env.NODE_TEST,
+  });
 });
 
 module.exports = router;
