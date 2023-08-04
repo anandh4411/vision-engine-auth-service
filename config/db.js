ef7209a1dab6fe0f8e0b00be4e5d7a07057ba5b7
anandh4411 <anandh4411@gmail.com>
2023-08-04 22:46:23 +0530

    updated mongodb url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const url = process.env.MONGO_URL;
+const url = process.env.MONGO_URL + "/vision-engine";
 
 function connectToDatabase() {
   mongoose
