d010f13fbc64aaaf65bc7fc38ed2bf9673d4ee30
anandh4411 <anandh4411@gmail.com>
2023-08-05 13:33:06 +0530

    added env var to vercel - jwtkey

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({
-    message: "Hey, this is vision-engine's User Auth Microservice",
+    message: "Hey, this is vision-engine's User Auth Microservice.",
   });
 });
 
