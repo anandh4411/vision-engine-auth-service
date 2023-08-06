db9efa9cb83130ba730d729e9a8271eba9585a5a
anandh4411 <anandh4411@gmail.com>
2023-08-06 18:41:01 +0530

    extracted multer profile pic upload logic to seperate utility file

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,6 @@
 const auth = require("../middlewares/auth");
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
+const multerProfilePicUpload = require("../utils/profile-pic-upload");
 const UserController = require("../controllers/userController");
 const UserCreateController = require("../controllers/userCreateController");
 const UserUpdateController = require("../controllers/userUpdateController");
@@ -9,38 +8,10 @@ const UserDeleteController = require("../controllers/userDeleteController");
 
 const router = express.Router();
 
-const imageFilter = (req, file, cb) => {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const mimetype = filetypes.test(file.mimetype);
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-  if (mimetype && extname) return cb(null, true);
-  return cb(false);
-};
-
-// Multer configuration for profile picture upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/profile-pic/");
-  },
-  filename: (req, file, cb) => {
-    const fileName = Date.now() + "-" + file.originalname;
-    cb(null, fileName);
-  },
-});
-
-const upload = multer({ storage, fileFilter: imageFilter });
-
-// get all users
-router.get("/all", UserController.getAllUsers);
-
-// get one users
-router.get("/me", auth, UserController.getUserById);
-
 // create a user - get otp
 router.post(
   "/create",
-  upload.single("profile-pic"),
+  multerProfilePicUpload.single("profile-pic"),
   UserCreateController.createUser
 );
 
@@ -53,10 +24,16 @@ router.post("/otp/resend", UserCreateController.resendOtp);
 // discard create user
 router.post("/discard", UserCreateController.dicardCreateUser);
 
-// delete user
-router.delete("/delete", auth, UserDeleteController.deleteUser);
+// get all users
+router.get("/all", UserController.getAllUsers);
+
+// get one users
+router.get("/me", auth, UserController.getUserById);
 
 // update a user
 router.put("/", auth, UserUpdateController.updateUser);
+
+// delete user
+router.delete("/delete", auth, UserDeleteController.deleteUser);
 
 module.exports = router;
